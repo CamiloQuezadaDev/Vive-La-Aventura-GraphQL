e997e0b30ea96dcaccb15c50319d62b61e03dcb8
Camilo Quezada <camiloquezadadev@gmail.com>
2020-09-28 22:52:29 -0300

    get all subsidiaries of current company and subsidiaries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,10 +10,20 @@ module Types
     field :services , [Types::ServiceType], null: true 
 
     def services 
-      
       Service.all 
-      
     end 
+
+    field :subsidiaries_of_current_company, [Types::SubsidiaryType], null: true 
+
+    def subsidiaries_of_current_company
+      context[:current_user] && context[:current_user].admin?   ? context[:current_company].subsidiaries : []
+    end
+
+    field :subsidiaries, [Types::SubsidiaryType], null: true 
+
+    def subsidiaries
+      Subsidiary.all
+    end
 
   end
 end
