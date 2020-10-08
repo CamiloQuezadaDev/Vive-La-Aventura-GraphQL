8fa51f183ac9ea84bf08712ce53e0708acc8772a
Camilo Quezada <camiloquezadadev@gmail.com>
2020-10-08 13:13:04 -0300

    cors updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do  
-        origins ['http://127.0.0.1:3001', 'http://localhost:3001','https://vive-la-aventura-react-ce4bzuzlz.vercel.app/']
+        origins Rails.env.production? ? 'https://vive-la-aventura-react-ce4bzuzlz.vercel.app/' :  ['http://127.0.0.1:3001', 'http://localhost:3001']
 
         resource '*',
         headers: :any,
