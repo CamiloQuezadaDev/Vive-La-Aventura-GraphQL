2ebca7129771f7fb2ad5536375681cb975a083c6
Camilo Quezada <camiloquezadadev@gmail.com>
2020-10-08 13:09:28 -0300

    access to fetch to proyect

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do  
-        origins ['http://127.0.0.1:3001', 'http://localhost:3001']
+        origins ['http://127.0.0.1:3001', 'http://localhost:3001','https://vive-la-aventura-react-ce4bzuzlz.vercel.app/']
 
         resource '*',
         headers: :any,
